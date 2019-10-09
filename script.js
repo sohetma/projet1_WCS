bfb0421b4a9d5a3fc40ba85e21b7a1599110766b
Sohet Maxime <maxime.sohet.ms@gmail.com>
2019-10-09 10:55:17 +0200

    js ok

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.querySelector('img').onclick = function() {addDescription()};
-function addDescription()
-{
-  var tag = document.getElementById('description');
-    tag.classList.toggle('description');
+function myFunction(param) {
+  console.log(param.id);
+  var element = document.getElementById(param.id);
+  console.log(element);
+  element.classList.toggle("vision");
 }
